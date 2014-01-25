b929c829844832554df2335bad4b14e1ed8f88e7
Spl3en <spl3en.contact@gmail.com>
2014-01-25 18:14:26 +0100

    First commit.

---
+++ MemBuffer.c
@@ -0,0 +1,59 @@
+#include "MemBuffer.h"
+
+MemBuffer *
+membuffer_new (DWORD addr, unsigned char *code, int size)
+{
+	MemBuffer *membuffer;
+
+	if ((membuffer = malloc(sizeof(MemBuffer))) == NULL)
+		return NULL;
+
+	membuffer_init (membuffer, addr,
+		buffer_new_from_ptr(code, size)
+	);
+
+	return membuffer;
+}
+
+MemBuffer *
+membuffer_new_from_buffer (DWORD addr, Buffer *buf)
+{
+	MemBuffer *membuffer;
+
+	if ((membuffer = malloc(sizeof(MemBuffer))) == NULL)
+		return NULL;
+
+	membuffer_init (membuffer, addr, buf);
+
+	return membuffer;
+}
+
+MemBuffer *
+membuffer_new_from_buffer_dup (DWORD addr, Buffer *buf)
+{
+	MemBuffer *membuffer;
+
+	if ((membuffer = malloc(sizeof(MemBuffer))) == NULL)
+		return NULL;
+
+	membuffer_init (membuffer, addr, buffer_dup(buf));
+
+	return membuffer;
+}
+
+void
+membuffer_init (MemBuffer *mb, DWORD addr, Buffer *buf)
+{
+	mb->addr = addr;
+	mb->buffer = buf;
+}
+
+void
+membuffer_free (MemBuffer *membuffer)
+{
+	if (membuffer != NULL)
+	{
+		buffer_free(membuffer->buffer);
+		free (membuffer);
+	}
+}
